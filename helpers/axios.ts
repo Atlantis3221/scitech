b764fd0f68a8452dd9e689e5c6a7a87664af495c
Bratishques <gurda999@gmail.com>
2021-08-17 11:45:08 +0000

    Add/project modals

--- helpers/axios.ts
+++ helpers/axios.ts
@@ -5,6 +5,10 @@ const ALL_GRANTS = 'http://localhost:3000/api/grants';
 const ALL_SORTED_NEWS = 'http://localhost:3000/api/news?sort_by=fields.date&sort_order=-1';
 const DEFINED_NEWS = 'http://localhost:3000/api/news?index_key=fields.url&index_value=';
 
+const instatnce = axios.create({
+	baseURL: "http://localhost:3000"
+})
+
 
 export const getContentfulNews = async() => await axios
 	.get(ALL_SORTED_NEWS)
@@ -38,7 +42,7 @@ export const getContentfulGrants = async() => await axios
 		console.log(error);
 	});
 
-export const getAllRequestData = async(project) => await axios
+export const getAllRequestData = async(project) => await instatnce
 	.get(`/api/reg/${project}`)
 	.then((res) => {
 		return res.data;
